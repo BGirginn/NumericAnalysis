da42027b758d061f8b47e2e6929dc01abf2466d9
BGirginn <girginbora30@gmail.com>
2025-03-12 17:44:34 +0300

    4-1 update

--- dorduncu_hafta.m
+++ dorduncu_hafta.m
@@ -19,11 +19,39 @@
 % standart polinom gösterimi an*x^n+a(n-1)*x^(n-1)+...+a1*x+a0
 
 % Eğer "x" daha önce tanımlanmmış bir sayı ise B(x) eşitliği alttaki şekilde yazılır
-B(x)=3*x^4-7*x^2+2*x-1
+% B(x)=3*x^4-7*x^2+2*x-1
 
 % Ancak eğer "x" tanımlanmış bir vektör ise alttaki gibi yazılır !!! Önemli fark !!!
 
-B(x)= 3*x.^4-7*x.^2+2*x-1  % bunun sonunda B ifadesi x vektörü ile aynı boyutta olacaktır
+% B(x)= 3*x.^4-7*x.^2+2*x-1  % bunun sonunda B ifadesi x vektörü ile aynı boyutta olacaktır
+
+% y(x)=4x^3+2x^2-7x+1
+
+x = linspace (-2,2,50); % -2 den 2 ye 50 tane eleman olacak şekilde yatay eksen
+a=[4 2 -7 1];  % buradaki sayıları sırası ile x yerine koyuyor
+y = polyval (a,x)
+plot(x,y,"k-");
+title("y=4x^3+2x^2-7x+1"),
+xlabel("x")
+ylabel("y(x)")
+grid
+
+% IKI POLINOMUN TOPLAMI VEYA FARKI
+
+% İki polinom toplanır veya çıkarılırken katsayıları içeren vektörlerin boyutları aynı olmalıdır.
+% İşlem yapılırken aynı üslü olanların katsayıları ile işlem yapılır
+% Eğer polinomlardan birinde olan üs diğerinde yoksa katsayısı 0 kabul edilerek matrise işlenir
+
+% Polinom bir tam sayı ile çarpılırken tüm ifadelerin katsayısını çarpar yerine yazarsın
 
 
+% K(x) = 4*x^4-8*x^3+7*x+1
+% M(x) = 2*x^5+7*x^3-2*x+6
 
+% T(x)=K(x)*M(x)
+
+k=[4 -8 0 7 1]
+m=[2 0 7 0 -2 6]
+t=conv(k,m)  % k ve m bir satır vektör olmak zorunda. 
+% bu komutu kullanmak için iki polinomun da (bulunmayıp katsayısı 0 olanlar bile dahil) katsayılırını birer matrise atamalısın
+% bu komut senin için çarpmayı yapıp istenen matrise atar (x^3 ve x^4 çarpımı katsayısı ile x^2 ve x^5 çarpımı katsayısı toplar)
